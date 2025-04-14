8b9691c2b9691e720685647b79c933c7834b5ac6
AAdele92 <adesewa.adeloye@gmail.com>
2025-04-14 18:05:36 +0100

    Update iam.tf

--- infra/deploy/iam.tf
+++ infra/deploy/iam.tf
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "cd" {
       "iam:PutUserPolicy",
       "iam:CreateAccessKey"
     ]
-    resources = ["arn:aws:s3:::${var.project_name}-cd"]
+    resources = ["arn:aws:iam:::user/${var.project_name}-cd"]
   }
 }
 
