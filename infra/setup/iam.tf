2a9d79f9bb7edbcb3d4ffd7ae01789ff094daea9
AAdele92 <adesewa.adeloye@gmail.com>
2025-04-17 23:09:33 +0100

    updated dynamo

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -62,6 +62,7 @@ data "aws_iam_policy_document" "tf_backend" {
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
 
+      "arn:aws:s3:::${var.bucket_name}/tf-state-setup/*",
       "arn:aws:s3:::${var.bucket_name}/tf-state-deploy/*",
       "arn:aws:s3:::${var.bucket_name}/tf-state-deploy-env/*"
     ]
