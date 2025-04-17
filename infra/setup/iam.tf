3490de636f731182a80161719383d73d846eb37b
AAdele92 <adesewa.adeloye@gmail.com>
2025-04-18 00:41:36 +0100

    s3-iam

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -41,7 +41,7 @@ resource "aws_iam_access_key" "cd" {
 data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect    = "Allow"
-    actions   = ["s3:ListBucket"]
+    actions   = ["s3:ListBucket", "s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = ["arn:aws:s3:::${var.bucket_name}"]
   }
 
