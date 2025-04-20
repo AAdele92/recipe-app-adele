01632194911d2a99cf0158a2e0581baadf96d67c
AAdele92 <adesewa.adeloye@gmail.com>
2025-04-20 13:33:21 +0100

    added rds

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -58,3 +58,9 @@ variable "db_username" {
 variable "db_password" {
   description = "Password for the Terraform database"
 }
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+  type        = string
+  default     = "recipe-app-3-ecr-proxy_image"
+}
